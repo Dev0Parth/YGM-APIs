56e85719874a86c353370716b76ed78501c19158
Dev0Parth <101238244+Dev0Parth@users.noreply.github.com>
2023-04-25 15:46:06 +1000

    Delete passwordHash.php

--- passwordHash.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $plain_password = "hr@12345";
-    $option = ['cost' => 12];
-    $hashed_password = password_hash($plain_password, PASSWORD_BCRYPT, $option);
-    $boolean = password_verify($plain_password, $hashed_password);
-    echo "Hashed password: " . $hashed_password;
-?>
